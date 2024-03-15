428705b02546cd8c193bf80f830af3f5f066fb04
shubham321-ux <shubham4260shubhu@gmail.com>
2024-03-15 01:39:38 -0700

    add colouns

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
-
-const express=require('express')
-const cors = require('cors')
-const bodyParser=require('body-parser')
-const mongoose=require('mongoose')
+require('dotenv').config();
+const express=require('express');
+const cors = require('cors');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
 const PORT = process.env.PORT || 8080;
 
 const app=express()
@@ -13,7 +12,7 @@ app.use(cors())
 const main=async()=>{
     await mongoose.connect(process.env.MONGO_URL)
     console.log("DB connected")
-}
+};
 main()
 const userSchema = mongoose.Schema({
     Gmail:String,
@@ -21,18 +20,19 @@ const userSchema = mongoose.Schema({
     Subject:String,
     Phone:Number,
     Massage:String
-})
+});
 
-const model=mongoose.model('userdata',userSchema)
+const model=mongoose.model('userdata',userSchema);
 
 
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.send("welcome")
-})
+});
 app.post('/demo',async(req,res)=>{
+    console.log("")
     let resdata= model()
     resdata.Gmail=req.body.email
     resdata.Name=req.body.name
@@ -44,12 +44,12 @@ app.post('/demo',async(req,res)=>{
 
     console.log(req.body)
     res.send(req.body)
-})
+});
 app.get('/geted',async(req,res)=>{
   const dataload=await model.findOne({Name:"SHUBHAM"})
   res.send(dataload)
   console.log(dataload)
-})
+});
 app.listen(PORT,()=>{
     console.log("server started on " ,PORT)
-})
+});
